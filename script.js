123056fddf972d51b2d45c637d2ac359e5ac8149
ImanRS1 <omid-omega@mail.com>
2020-08-25 19:53:49 +0200

    added win logic

--- script.js
+++ script.js
@@ -28,10 +28,76 @@ function checkForWinner(theGrid){
        //console.log(theGrid.children[i].innerHTML);
         //console.log(theGrid.children[i].id)
 
-        var bokstav = theGrid.children[i].id;
-        console.log(bokstav.charAt(1));
+        var thisGrid = theGrid.children[i].id;
+        console.log(thisGrid.charAt(1));
+
+        var thisValue = theGrid.children[i].innerHTML;
+        console.log(thisValue);
+
+        if(thisGrid.charAt(1) == 1){
+            
+        }
+
+        if(checkRows(theGrid)){
+            return;
+        }
+
+        if(checkColumns(theGrid)){
+            return;
+        }
+
+        if(checkDiagonal(theGrid)){
+            return;
+        }
+/* 
+        if(theGrid.children[0].innerHTML == theGrid.children[1].innerHTML && theGrid.children[2].innerHTML == theGrid.children[1].innerHTML){
+            console.log("vinnare på rad 1");
+            return;
+        } */
+
 
     }
 
   
+}
+
+function checkRows(theGrid){
+    if(theGrid.children[0].innerHTML == theGrid.children[1].innerHTML && theGrid.children[2].innerHTML == theGrid.children[1].innerHTML && theGrid.children[2].innerHTML != ""){
+        console.log("vinnare på rad 1");
+        return true;
+    }
+    if(theGrid.children[3].innerHTML == theGrid.children[4].innerHTML && theGrid.children[5].innerHTML == theGrid.children[4].innerHTML && theGrid.children[5].innerHTML != ""){
+        console.log("vinnare på rad 2");
+        return true;
+    }
+    if(theGrid.children[6].innerHTML == theGrid.children[7].innerHTML && theGrid.children[8].innerHTML == theGrid.children[7].innerHTML && theGrid.children[8].innerHTML != ""){
+        console.log("vinnare på rad 3");
+        return true;
+    }
+}
+
+function checkColumns(theGrid){
+    if(theGrid.children[0].innerHTML == theGrid.children[3].innerHTML && theGrid.children[3].innerHTML == theGrid.children[6].innerHTML && theGrid.children[6].innerHTML != ""){
+        console.log("vinnare på kolumn 1");
+        return true;
+    }
+    if(theGrid.children[1].innerHTML == theGrid.children[4].innerHTML && theGrid.children[4].innerHTML == theGrid.children[7].innerHTML && theGrid.children[7].innerHTML != ""){
+        console.log("vinnare på kolumn 2");
+        return true;
+    }
+    if(theGrid.children[2].innerHTML == theGrid.children[5].innerHTML && theGrid.children[5].innerHTML == theGrid.children[8].innerHTML && theGrid.children[8].innerHTML != ""){
+        console.log("vinnare på kolumn 3");
+        return true;
+    }
+}
+
+function checkDiagonal(theGrid){
+    if(theGrid.children[0].innerHTML == theGrid.children[4].innerHTML && theGrid.children[4].innerHTML == theGrid.children[8].innerHTML && theGrid.children[8].innerHTML != ""){
+        console.log("vinnare på diagonal pekandes åt vänster");
+        return true;
+    }
+    if(theGrid.children[6].innerHTML == theGrid.children[4].innerHTML && theGrid.children[4].innerHTML == theGrid.children[2].innerHTML && theGrid.children[2].innerHTML != ""){
+        console.log("vinnare på diagonal pekades åt höger");
+        return true;
+    }
 }
