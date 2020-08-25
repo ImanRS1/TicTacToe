6703b0ba34b9e6ac28ddacb7f38760d6357bcaa0
ImanRS1 <omid-omega@mail.com>
2020-08-25 19:21:46 +0200

    insert X/O function and check for already marked

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
-console.log("hello")
+let i = 0;
 
 function testClick(event){
-    event.target.innerHTML ="X";  
+    if(alreadyMarked(event)){
+        return;
+    }else{
+    if(i % 2 === 0){
+        event.target.innerHTML ="X";
+    }else{
+        event.target.innerHTML ="O";
+    }  
+    i++;
+    }
+    let theGrid= document.getElementById("gridContainer");
+  
+    checkForWinner(theGrid);
+}
+
+function alreadyMarked(event){
+    if(event.target.innerHTML != ""){
+        return true;
+    }
+}
+
+function checkForWinner(theGrid){
+
+    for(var i = 0; i < 9; i++){
+       //console.log(theGrid.children[i].innerHTML);
+        //console.log(theGrid.children[i].id)
+
+        var bokstav = theGrid.children[i].id;
+        console.log(bokstav.charAt(1));
+
+    }
+
+  
 }
