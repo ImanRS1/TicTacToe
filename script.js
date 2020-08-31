feccff628a4d4a249f1e15bc151a7262d9f87058
ImanRS1 <omid-omega@mail.com>
2020-09-01 00:18:41 +0200

    winLine logic done, winning line glows red

--- script.js
+++ script.js
@@ -92,29 +92,46 @@ function checkRows(theGrid){
 
 function checkColumns(theGrid){
     if(theGrid.children[0].innerHTML == theGrid.children[3].innerHTML && theGrid.children[3].innerHTML == theGrid.children[6].innerHTML && theGrid.children[6].innerHTML != ""){
-  /*       theGrid.children[0, 3, 6].removeAttribute("class", "neon-text-blue");
-        theGrid.children[0, 3, 6].setAttribute("class", "neon-text-red"); */
-
-        theGrid.querySelectorAll('g11, g21, g31').removeAttribute("class", "neon-text-blue");
-        theGrid.querySelectorAll('g11, g21, g31').setAttribute("class", "neon-text-red");
-
-       /*  theGrid.getElementsByName('g11, g21, g31').removeAttribute("class", "neon-text-blue"); */
-
+        let winLine = document.querySelectorAll("#g11, #g21, #g31");
+        for(let y = 0; y<3; y++){
+            winLine[y].removeAttribute("class", "neon-text-blue");
+            winLine[y].setAttribute("class", "neon-text-red");
+        }
         return true;
     }
     if(theGrid.children[1].innerHTML == theGrid.children[4].innerHTML && theGrid.children[4].innerHTML == theGrid.children[7].innerHTML && theGrid.children[7].innerHTML != ""){
+        let winLine = document.querySelectorAll("#g12, #g22, #g32");
+        for(let y = 0; y<3; y++){
+            winLine[y].removeAttribute("class", "neon-text-blue");
+            winLine[y].setAttribute("class", "neon-text-red");
+        }
         return true;
     }
     if(theGrid.children[2].innerHTML == theGrid.children[5].innerHTML && theGrid.children[5].innerHTML == theGrid.children[8].innerHTML && theGrid.children[8].innerHTML != ""){
+        let winLine = document.querySelectorAll("#g13, #g23, #g33");
+        for(let y = 0; y<3; y++){
+            winLine[y].removeAttribute("class", "neon-text-blue");
+            winLine[y].setAttribute("class", "neon-text-red");
+        }
         return true;
     }
 }
 
 function checkDiagonal(theGrid){
     if(theGrid.children[0].innerHTML == theGrid.children[4].innerHTML && theGrid.children[4].innerHTML == theGrid.children[8].innerHTML && theGrid.children[8].innerHTML != ""){
+        let winLine = document.querySelectorAll("#g11, #g22, #g33");
+        for(let y = 0; y<3; y++){
+            winLine[y].removeAttribute("class", "neon-text-blue");
+            winLine[y].setAttribute("class", "neon-text-red");
+        }
         return true;
     }
     if(theGrid.children[6].innerHTML == theGrid.children[4].innerHTML && theGrid.children[4].innerHTML == theGrid.children[2].innerHTML && theGrid.children[2].innerHTML != ""){
+        let winLine = document.querySelectorAll("#g13, #g22, #g31");
+        for(let y = 0; y<3; y++){
+            winLine[y].removeAttribute("class", "neon-text-blue");
+            winLine[y].setAttribute("class", "neon-text-red");
+        }
         return true;
     }
 }
