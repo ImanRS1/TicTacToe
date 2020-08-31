31c070b7a591839f54b5c17935a2d54070804584
ImanRS1 <omid-omega@mail.com>
2020-08-31 23:03:08 +0200

    style/logic for winning row ongoing

--- script.js
+++ script.js
@@ -21,6 +21,8 @@ function clickedGrid(event){
 function resetGrid(){
     for(var x=0; x < theGrid.children.length; x++){
         theGrid.children[x].innerHTML = "";
+        theGrid.children[x].removeAttribute("class", "neon-text-red");
+        theGrid.children[x].setAttribute("class", "neon-text-blue");
     }
     winAnnouncer.innerHTML = "Lets play";
     i = 0;
@@ -66,18 +68,38 @@ function checkForWinner(theGrid){
 
 function checkRows(theGrid){
     if(theGrid.children[0].innerHTML == theGrid.children[1].innerHTML && theGrid.children[2].innerHTML == theGrid.children[1].innerHTML && theGrid.children[2].innerHTML != ""){
+        for(var y = 0; y<3; y++){
+            theGrid.children[y].removeAttribute("class", "neon-text-blue");
+            theGrid.children[y].setAttribute("class", "neon-text-red");
+        }
         return true;
     }
     if(theGrid.children[3].innerHTML == theGrid.children[4].innerHTML && theGrid.children[5].innerHTML == theGrid.children[4].innerHTML && theGrid.children[5].innerHTML != ""){
+        for(var y = 3; y<6; y++){
+            theGrid.children[y].removeAttribute("class", "neon-text-blue");
+            theGrid.children[y].setAttribute("class", "neon-text-red");
+        }
         return true;
     }
     if(theGrid.children[6].innerHTML == theGrid.children[7].innerHTML && theGrid.children[8].innerHTML == theGrid.children[7].innerHTML && theGrid.children[8].innerHTML != ""){
+        for(var y = 6; y<9; y++){
+            theGrid.children[y].removeAttribute("class", "neon-text-blue");
+            theGrid.children[y].setAttribute("class", "neon-text-red");
+        }
         return true;
     }
 }
 
 function checkColumns(theGrid){
     if(theGrid.children[0].innerHTML == theGrid.children[3].innerHTML && theGrid.children[3].innerHTML == theGrid.children[6].innerHTML && theGrid.children[6].innerHTML != ""){
+  /*       theGrid.children[0, 3, 6].removeAttribute("class", "neon-text-blue");
+        theGrid.children[0, 3, 6].setAttribute("class", "neon-text-red"); */
+
+        theGrid.querySelectorAll('g11, g21, g31').removeAttribute("class", "neon-text-blue");
+        theGrid.querySelectorAll('g11, g21, g31').setAttribute("class", "neon-text-red");
+
+       /*  theGrid.getElementsByName('g11, g21, g31').removeAttribute("class", "neon-text-blue"); */
+
         return true;
     }
     if(theGrid.children[1].innerHTML == theGrid.children[4].innerHTML && theGrid.children[4].innerHTML == theGrid.children[7].innerHTML && theGrid.children[7].innerHTML != ""){
